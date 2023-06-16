ac48628f77b459a89e9ad9a10c355907273c974f
MimBinFikret <muhammed.samil.albayrak@gmail.com>
2023-06-16 09:02:06 +0300

    Iyilestirme

--- math-T/math_T.py
+++ math-T/math_T.py
@@ -1,39 +1,35 @@
-def mimLine(name, inEnd=1, outEnd=1, inDimension=2, outDimension=2):
-    name = name
-    centerArray = [1]
-    info=f"""
-    name = {name}
-    
-    inEnd = {inEnd}
-    outEnd = {outEnd}
-    
-    inDimension = {inDimension}
-    outDimension = {outDimension}
-    """
-    print(info)
+﻿class MimLine:
+    def __init__(self, name, inEnd=1, outEnd=1, inDimension=2, outDimension=2):
+        self.name = name
+        self.centerArray = [1]
+        self.info=f"""
+        name = {name}
+        inEnd = {inEnd}
+        outEnd = {outEnd}
+        inDimension = {inDimension}
+        outDimension = {outDimension}
+        """
+        print(self.info)
 
-    for i in range(1, inEnd+1):
-        value = -(i+1)
-        centerArray.append(value)
-    centerArray.sort()
+        for i in range(1, inEnd+1):
+            value = -(i+1)
+            self.centerArray.append(value)
+        self.centerArray.sort()
 
-    for i in range(outEnd):
-        centerArray.append(i+2)
-    centerArray.sort()
+        for i in range(outEnd):
+            self.centerArray.append(i+2)
+        self.centerArray.sort()
 
-    print(' '.join(str(i) for i in centerArray))
-    
-    d_centerArray = centerArray
-    merkez = d_centerArray.index(1)
-    for i in range(inEnd+1):
-        d_centerArray[merkez-i] = [i for i in range(inDimension)]
+        self.d_centerArray = self.centerArray.copy()
+        merkez = self.d_centerArray.index(1)
+        for i in range(inEnd+1):
+            self.d_centerArray[merkez-i] = [i for i in range(inDimension)]
 
-    for i in range(outEnd+1):
-        d_centerArray[merkez+i] = [i for i in range(outDimension)]
-
-    print(' '.join(str(i) for i in d_centerArray))
+        for i in range(outEnd+1):
+            self.d_centerArray[merkez+i] = [i for i in range(outDimension)]
 
 
-
-mimLine("kapi", inEnd=3, outEnd=5)
-mimLine("deve", inEnd=1, outEnd=4, inDimension=2, outDimension=6)
+# Örnek kullanım
+mim_line = MimLine("Su", inEnd=2, outEnd=2, inDimension=3, outDimension=3)
+print(mim_line.centerArray)
+print(mim_line.d_centerArray)
